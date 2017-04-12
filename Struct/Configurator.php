30e5c9269cc059c59d4065e9cff68714dec6d9d6
kekster <e.warneke@aquatuning.de>
2017-04-12 10:10:32 +0200

    added unused configurator struct

---
+++ Struct/Configurator.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * Aquatuning Software Development - Configurator - Struct
+ *
+ * @category  Aquatuning
+ * @package   Shopware\Plugins\AtsdConfigurator
+ * @copyright Copyright (c) 2015, Aquatuning GmbH
+ */
+
+namespace Shopware\AtsdConfigurator\Struct;
+
+/**
+ * Aquatuning Software Development - Configurator - Struct
+ */
+
+class Configurator
+{
+
+    /**
+     * ...
+     *
+     * @var integer
+     */
+
+    protected $id;
+
+
+
+    /**
+     * ...
+     *
+     * @var string
+     */
+
+    protected $name;
+
+
+
+
+
+
+
+    /**
+     * ...
+     *
+     * @var boolean
+     */
+
+    protected $isValidated = false;
+
+
+
+    /**
+     * ...
+     *
+     * @var boolean
+     */
+
+    protected $isParsed = false;
+
+
+
+}
+
+
+
